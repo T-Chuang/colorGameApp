653c8c7cce01fd27898c1c42c68eb629e157da86
Theodore Chuang <chuangtheodore@gmail.com>
2017-07-04 23:39:24 +1000

    Added basic functionality to app. Colors are randomized. Win/Lose logic.

--- colorGame.js
+++ colorGame.js
@@ -1,17 +1,37 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
-
-var squares= document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var h1 = document.querySelector("h1");
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var squares= document.querySelectorAll(".square");
+var colors = generateRandomColors(6);
+var pickedColor = pickedColor();
+
 
 colorDisplay.textContent = pickedColor;
+
+function generateRandomColors(num){
+    //create an array
+    var arr = [];
+    //repeat num times
+    for(var i = 0; i < num; i++){
+        //generate random color and push into arr
+        arr.push(randomColor());
+    }
+    //return array
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function pickedColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
 
 for(var i = 0; 0 < squares.length; i++){
     //add initial colors to squares
@@ -23,9 +43,19 @@ for(var i = 0; 0 < squares.length; i++){
         var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
         if(clickedColor === pickedColor){
-            alert("win"); 
+            messageDisplay.textContent = "Correct!";
+            changeAllColors(pickedColor);
+            h1.style.backgroundColor = pickedColor;
         } else {
-            alert("lose");
+            this.style.backgroundColor = document.body.style.backgroundColor;
+            messageDisplay.textContent = "Try Again";
         }
     });
+}
+
+function changeAllColors(color){
+    //loop all squares
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
 }
